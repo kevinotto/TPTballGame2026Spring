1b39ac4a5ae1cfc57f395c8aca25934e4e59bfd6
kevinotto <ottokoit@gmail.com>
2026-02-10 10:16:49 +0200

    another commit

---
+++ BallGameSpring2026/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed;
+    public TMP_Text ScoreText;
+    public TMP_Text WinText;
+    public GameObject Gate;
+    private Rigidbody rb;
+    public int Score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Score = 0;
+        SetScoreText();
+        WinText.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * Speed);
+
+        //Restart level
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+        //Quit game
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("coin"))
+        {
+            other.gameObject.SetActive(false);
+            Score++;
+            SetScoreText();
+            if (Score >= 5)
+            {
+                Gate.gameObject.SetActive(false);
+            }
+        }
+
+        if (other.gameObject.CompareTag("danger"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    void SetScoreText()
+    {
+        ScoreText.text = "Score: " + Score.ToString();
+
+        if(Score == 10)
+        {
+            WinText.text = "You win! Press R to restart or ESC to quit";
+        }
+    }
+}
